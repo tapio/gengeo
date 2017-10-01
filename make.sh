91a6121ce041f50ea193ce94079790e308955945
Tapio Vierros <tapiovierros@gmail.com>
2017-10-01 19:19:46 +0300

    Make.sh can build in separate build directory.

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ROOT="$(dirname "$(readlink -f "$0")")"
 EXENAME="gengeo"
 CFLAGS="-std=c++11 -O2 -march=native -Wall -Wextra -Wno-unused-parameter"
 
@@ -7,12 +8,12 @@ if [ "x$CXX" = "x" ]; then
 	# Default to GCC
 	CXX=g++
 	# Prefer clang++ if available
-	which clang++ > /dev/null
+	which clang++ > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]; then
 		CXX=clang++
 	fi
 fi
 
 set -x
-$CXX $CFLAGS src/*.cpp -o $EXENAME
+$CXX $CFLAGS "$ROOT"/src/*.cpp -o $EXENAME
 
